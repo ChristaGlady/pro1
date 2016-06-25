77bd1d6318a5c34bb0990fc2707f23956853f987
ChristaGlady <christajglady7@gmail.com>
2016-06-25 07:30:37 +0530

    Update PosNeg0.c

--- PosNeg0.c
+++ PosNeg0.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+clrscr();
+printf("Enter any number: ");
+scanf("%d", &num);
+ if(num > 0)
+{
+printf("Number is POSITIVE");
+}
+else if(num < 0)
+{
+printf("Number is NEGATIVE");
+}
+else
+{
+printf("Number is ZERO");
+}
+getch();
+return 0;
+} 
 
